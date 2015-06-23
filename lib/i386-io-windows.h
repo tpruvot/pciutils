c4ed58661a0047ac72c6c9cb64f04779252d4f72
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2015-06-23 03:27:15 +0200

    winio: init and shutdown properly
    do not use conio functions... use the winio exports

--- lib/i386-io-windows.h
+++ lib/i386-io-windows.h
@@ -5,58 +5,69 @@
  *	Copyright (c) 2006 Martin Mares <mj@ucw.cz>
  *
  *	Can be freely distributed and used under the terms of the GNU GPL.
+ *
+ *	Changes:
+ *	2015-06-23: Proper implementation (tpruvot)
+ *
  */
 
-#include <io.h>
+#define WIN32_LEAN_AND_MEAN
+#define NOGDI
 #include <windows.h>
 
-#ifndef __GNUC__
-#include <conio.h>
+/* WinIo declarations */
+typedef BOOL bool;
+
+typedef bool (_stdcall *INITIALIZEWINIO)(void);
+typedef void (_stdcall *SHUTDOWNWINIO)(void);
+typedef bool (_stdcall *GETPORTVAL)(WORD, PDWORD, BYTE);
+typedef bool (_stdcall *SETPORTVAL)(WORD, DWORD, BYTE);
+
+INITIALIZEWINIO InitializeWinIo;
+SHUTDOWNWINIO ShutdownWinIo;
+GETPORTVAL GetPortVal;
+SETPORTVAL SetPortVal;
+
+#ifdef _WIN64
+#define WINIO_LIB_NAME_V3 "WinIo64.dll"
 #else
-int _outp(unsigned short port, int databyte);
-unsigned short _outpw(unsigned short port, unsigned short dataword);
-unsigned long _outpd(unsigned short port, unsigned long dataword);
-int _inp(unsigned short port);
-unsigned short _inpw(unsigned short port);
-unsigned long _inpd(unsigned short port);
+#define WINIO_LIB_NAME_V3 "WinIo32.dll"
 #endif
 
-#define outb(x,y) _outp(y,x)
-#define outw(x,y) _outpw(y,x)
-#define outl(x,y) _outpd(y,x)
+#define GETPROC(n,d) \
+  n = (d) GetProcAddress(lib, #n); \
+  if (!n) { \
+    a->warning("i386-io-windows: Couldn't find " #n " function."); \
+    return 0; \
+  }
 
-#define inb(x) _inp(x)
-#define inw(x) _inpw(x)
-#define inl(x) _inpd(x)
+static HMODULE lib = NULL;
 
 static int
 intel_setup_io(struct pci_access *a)
 {
-  typedef int (*MYPROC)(void);
-  MYPROC InitializeWinIo;
-  HMODULE lib;
-
-  lib = LoadLibrary("WinIo.dll");
+  // Check if already loaded
   if (!lib)
-    {
-      a->warning("i386-io-windows: Couldn't load WinIo.dll.");
-      return 0;
-    }
-  /* XXX: Is this really needed? --mj */
-  GetProcAddress(lib, "InitializeWinIo");
+  {
+    lib = LoadLibrary(WINIO_LIB_NAME_V3);
+    if (!lib) /* WinIo 3 loading failed, try loading WinIo 2 */
+      lib = LoadLibrary("WinIo.dll");
 
-  InitializeWinIo = (MYPROC) GetProcAddress(lib, "InitializeWinIo");
-  if (!InitializeWinIo)
-    {
-      a->warning("i386-io-windows: Couldn't find InitializeWinIo function.");
+    if (!lib) {
+      a->warning("i386-io-windows: Neither " WINIO_LIB_NAME_V3 " or WinIo.dll could be loaded.");
       return 0;
     }
 
-  if (!InitializeWinIo())
-    {
-      a->warning("i386-io-windows: InitializeWinIo() failed.");
-      return 0;
-    }
+    GETPROC(InitializeWinIo, INITIALIZEWINIO);
+    GETPROC(ShutdownWinIo, SHUTDOWNWINIO);
+    GETPROC(GetPortVal, GETPORTVAL);
+    GETPROC(SetPortVal, SETPORTVAL);
+  }
+
+  if (!InitializeWinIo()) {
+    a->warning("i386-io-windows: IO library initialization failed. Try running from an elevated command prompt.");
+    return 0;
+  }
 
   return 1;
 }
@@ -64,6 +75,67 @@ intel_setup_io(struct pci_access *a)
 static inline int
 intel_cleanup_io(struct pci_access *a UNUSED)
 {
-  //TODO: DeInitializeWinIo!
-  return 1;
+  if (!lib)
+    return 0;
+  ShutdownWinIo();
+  FreeLibrary(lib);
+  lib = NULL;
+  return -1;
+}
+
+static inline u8
+inb (u16 port)
+{
+  if (lib) {
+    DWORD pv;
+    if (GetPortVal(port, &pv, 1))
+      return (u8)pv;
+  }
+  return 0;
+}
+
+static inline u16
+inw (u16 port)
+{
+  if (lib) {
+    DWORD pv;
+    if (GetPortVal(port, &pv, 2))
+      return (u16)pv;
+  }
+  return 0;
+}
+
+static inline u32
+inl (u16 port)
+{
+  if (lib) {
+    DWORD pv;
+    if (GetPortVal(port, &pv, 4))
+      return (u32)pv;
+  }
+  return 0;
+}
+
+static inline void
+outb (u8 value, u16 port)
+{
+  if (lib) {
+    SetPortVal(port, value, 1);
+  }
+}
+
+static inline void
+outw (u16 value, u16 port)
+{
+  if (lib) {
+    SetPortVal(port, value, 2);
+  }
+}
+
+static inline void
+outl (u32 value, u16 port)
+{
+  if (lib) {
+    SetPortVal(port, value, 4);
+  }
 }
